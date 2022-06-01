c4beda70004f780aaa94bbbc21b9fe6054b8353b
johntarus <tarusjohn96@gmail.com>
2022-06-02 01:11:39 +0300

    cards

--- src/components/Home/Featured.js
+++ src/components/Home/Featured.js
@@ -3,23 +3,23 @@ import product from "../../assets/images/product.jpg";
 
 const Featured = () => {
   return (
-    <div className="bg-[#F5F5F5] px-20 mb-8 pb-12">
+    <div className="bg-[#F5F5F5] px-20 sm:px-6 mb-8 pb-12">
       <h1 className="text-center font-bold text-4xl pt-24 pb-12 mb-6">
         Featured Products
       </h1>
       <div className="grid grid-cols-3 gap-6 divide-x content-center text-center font-semibold py-8 sm:grid-cols-1 md:grid-cols-2 pb-16">
         <div>
-          <img src={product} alt="T Shirt" />
+          <img className="mb-4" src={product} alt="T Shirt" />
           <p>T Shirt</p>
           <p>Ksh. 100</p>
         </div>
         <div>
-          <img src={product} alt="T Shirt" />
+          <img className="mb-4" src={product} alt="T Shirt" />
           <p>T Shirt</p>
           <p>Ksh. 100</p>
         </div>
         <div>
-          <img src={product} alt="T Shirt" />
+          <img className="mb-4" src={product} alt="T Shirt" />
           <p>T Shirt</p>
           <p>Ksh. 100</p>
         </div>
--- src/components/Home/History.js
+++ src/components/Home/History.js
@@ -2,13 +2,46 @@ import React from "react";
 
 const History = () => {
   return (
-    <div className="bg-[#ECC488] px-20 md:px-4 sm:px-8 mb-4 pb-20">
+    <div className="bg-[#ECC488] px-36 md:px-4 sm:px-8 mb-4 pb-20">
       <div className=" grid grid-cols-3 sm:grid-cols-1 sm:gap-4 gap-12 pt-20 mb-8 sm:pt-24">
         <div className="font-semibold text-2xl md:text-xl">
           <h1>Explore Our Products to Discover Crazy Deals</h1>
         </div>
         <div className="col-span-2 text-xl text-[#525557]">
           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-8 place-items-center text-center md:grid-cols-2 sm:grid-cols-1">
+        <div className="bg-[#886F6F] w-68 p-8 rounded-md">
+          <i className="fa fa-compass fa-3x mb-4" aria-hidden="true"></i>
+          <h1 className="font-bold text-3xl pb-4">Mission</h1>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </div>
+
+        <div className="bg-[#886F6F] w-84 p-8 rounded-md">
+          <i className="fa fa-eye fa-3x mb-6" aria-hidden="true"></i>
+          <h1 className="font-bold text-3xl pb-4">Vission</h1>
+          <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </div>
+        <div className="bg-[#886F6F] w-84 p-8 rounded-md">
+          <i
+            className="fa fa-history fa-3x mb-6 color-white"
+            aria-hidden="true"
+          ></i>
+          <h1 className="font-bold text-3xl pb-4">History</h1>
+          <p className="text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
             dolorum debitis consectetur reprehenderit non aliquam voluptates
             dolore aut vero consequuntur.
