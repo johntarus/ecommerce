b5d2f688780c650a28c07bff98745b9a5527e748
johntarus <tarusjohn96@gmail.com>
2022-06-01 15:02:57 +0300

    overview

---
+++ src/components/Home/Overview.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Overview = () => {
+  return (
+    <div>
+      <div>
+        <h1>Overview</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
 import Benefits from "../components/Home/Benefits";
+import Overview from "../components/Home/Overview";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Benefits />
+      <Overview />
     </>
   );
 };
