d9b5bd620f13791d17b122058e5227bd9828cfcf
johntarus <tarusjohn96@gmail.com>
2022-06-02 23:09:28 +0300

    footer

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -4,10 +4,9 @@ const Footer = () => {
   return (
     <footer className="h-16 bg-[#EDE6DB] flex items-center justify-center">
       <h1 className="text-center font-bold">
-        `&copy; {new Date().getFullYear()}`
-        <span className="text-[#ED4833]"> FASHOP</span>
+        © 2022 <span className="text-[#ED4833]">FASHOP</span> || All Rights
+        Reserved
       </h1>
-      <h1 className="ml-2 font-bold">|| All Rights Reserved</h1>
     </footer>
   );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,7 @@
 import React from "react";
 
-function About() {
-  return (
-    <div>
-      <div>
-        <h1>About</h1>
-        <p className="pt-4 text-lg">
-          Designed for the jet-setter in mind, our shirt is the ideal blend of
-          comfort and style. Built for the man who’s in demand, this classic
-          design will keep you looking presentable on the go, with a gentle
-          fabric for all-day comfort. Because you won’t perform at your best if
-          you’re irritated by your clothing. Our shirt is a sleek take on a
-          well-loved classic featuring a tried-and-tested spread collar and a
-          slim fit styling.
-        </p>
-        <p className="pt-4 text-lg">
-          Designed for the jet-setter in mind, our shirt is the ideal blend of
-          comfort and style. Built for the man who’s in demand, this classic
-          design will keep you looking presentable on the go, with a gentle
-          fabric for all-day comfort. Because you won’t perform at your best if
-          you’re irritated by your clothing. Our shirt is a sleek take on a
-          well-loved classic featuring a tried-and-tested spread collar and a
-          slim fit styling.
-        </p>
-      </div>
-    </div>
-  );
-}
+const About = () => {
+  return <div>About</div>;
+};
 
 export default About;
