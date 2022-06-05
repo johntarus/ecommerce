f26f41dc14e4bc4bde9264870928ca3242a1c478
johntarus <tarusjohn96@gmail.com>
2022-06-05 23:40:16 +0300

    products

--- src/components/helpers/data.js
+++ src/components/helpers/data.js
@@ -1,18 +1,21 @@
 import blackdress from "../../assets/images/black-dress.jpg";
+import blackshirt from "../../assets/images/black-shirt.jpg";
 
 export const products = [
   {
     id: 1,
     name: "Black Dress",
-    price: "Ksh. 100",
-    description: "lorem ipsum",
+    price: "Ksh. 600",
+    description:
+      "Show your curves some love with our Most elegant Bodycon dresses.",
     image: blackdress,
   },
   {
     id: 2,
-    name: "Product 2",
-    price: "$200",
-    description: "lorem ipsum",
-    image: "https://picsum.photos/200/300",
+    name: "Black Shirt",
+    price: "Ksh. 900",
+    description:
+      "We provide solid color t-shirts with 100% cotton combined 30s best quality for solid color for quality misty materials",
+    image: blackshirt,
   },
 ];
