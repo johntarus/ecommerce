4f4b9ae6be4a059b796f899ef84e5a212ab365eb
johntarus <tarusjohn96@gmail.com>
2022-06-30 23:11:47 +0300

    making footer stick to the bottom of each page

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="h-16 bg-[#EDE6DB] flex items-center justify-center">
+    <footer className="position-fixed bottom-0 h-16 bg-[#EDE6DB] flex items-center justify-center">
       <h1 className="text-center font-bold xs:text-sm">
         © 2022 <span className="text-[#ED4833]">FASHOP</span> || All Rights
         Reserved
