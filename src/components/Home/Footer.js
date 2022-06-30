dd4858fcf1dbd58afd30459b0ad3d2716f9765d2
johntarus <tarusjohn96@gmail.com>
2022-07-01 00:04:08 +0300

    making sign up page responsive

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full">
+    <div className="fixed bottom-0 w-full mt-6">
       <footer className="flex h-16 bg-[#EDE6DB] items-center justify-center">
         <h1 className="text-center font-bold xs:text-sm">
           © 2022 <span className="text-[#ED4833]">FASHOP</span> || All Rights
