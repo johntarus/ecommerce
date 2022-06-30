c058ace810a69eaf6f59b615b569d7d4fc56f41d
johntarus <tarusjohn96@gmail.com>
2022-07-01 00:02:10 +0300

    cart

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full">
-      <div className="flex h-16 bg-[#EDE6DB] items-center justify-center">
+    <div className="fixed bottom-0 w-full">
+      <footer className="flex h-16 bg-[#EDE6DB] items-center justify-center">
         <h1 className="text-center font-bold xs:text-sm">
           © 2022 <span className="text-[#ED4833]">FASHOP</span> || All Rights
           Reserved
         </h1>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
 
