7cdf767499c77f566da9817fbf63189892a9f81f
johntarus <tarusjohn96@gmail.com>
2022-08-26 12:26:19 +0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
-module.exports {
+module.export = {
   env: {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    '@salesforce/eslint-config-lwc/recommended'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
